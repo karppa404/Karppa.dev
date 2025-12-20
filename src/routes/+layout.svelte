1b12bdec892ddc5e940e030b81f58fce3c0cc42d
Abel Semahegn <abel249733@gmail.com>
2025-12-20 12:54:17 -0600

    forgor to get rid of the tanstack query client

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
     import './layout.css';
     import favicon from '$lib/assets/tiny.png';
     import { ModeWatcher } from "mode-watcher";
-    import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+
 
     let { children } = $props();
-    const queryClient = new QueryClient()
+
 
 </script>
 
@@ -14,7 +14,4 @@
 	<title>Karppa.dev</title>
 </svelte:head>
 <ModeWatcher />
-
-<QueryClientProvider client={queryClient}>
     {@render children?.()}
-</QueryClientProvider>
