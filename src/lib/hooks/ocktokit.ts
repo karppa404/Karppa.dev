6572af282979683ad30df05eb557f386778dbc89
Abel Semahegn <abel249733@gmail.com>
2026-05-19 11:15:25 -0500

    FINAL REDO FOREVER

--- src/lib/hooks/ocktokit.ts
+++
@@ -1,4 +0,0 @@
-import { Octokit } from "octokit";
-import { env } from "$env/dynamic/private";
-// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-export const octokit = new Octokit({ auth: env.PRIVATE_GH_TOKEN});
